aeb35fc9bb0424620d5d59399fc932ab7f8425dd
Oleksandr Yesipov <oleksandr.yesipov@avanquest.com>
2022-08-12 19:33:03 +0300

    add pop-up

--- AIStore.Web/wwwroot/js/common/pop-up.js
+++ AIStore.Web/wwwroot/js/common/pop-up.js
@@ -1,1 +1,9 @@
-﻿
+﻿function CloseLoginModal() {
+let popup = document.getElementById('popupLogin');
+  popup.style.display = "none";
+}
+
+function ShowLoginModal() {
+    let popup = document.getElementById('popupLogin');
+    popup.style.display = "block";
+}
